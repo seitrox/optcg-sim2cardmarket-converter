f2b57bd6758f618ab933652cfa82ce741769571b
seitrox <20080181+seitrox@users.noreply.github.com>
2025-03-13 00:56:15 +0100

    more fixes

--- src/config.py
+++ src/config.py
@@ -3,5 +3,5 @@ from urllib.parse import urljoin
 
 BASE_URL="https://en.onepiece-cardgame.com"
 CARD_LIST_URL= urljoin(BASE_URL, "cardlist/")
-RAW_DATA_DIR = Path("../raw_data")
-RESULTS_DIR = Path("../results")
+RAW_DATA_DIR = Path("raw_data")
+RESULTS_DIR = Path("results")
